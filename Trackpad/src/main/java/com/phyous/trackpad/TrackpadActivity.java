fd21359bc4195c71e8e5ac54bbf915b97071f808
Philip Youssef <pyoussef@twitter.com>
2013-09-06 20:23:12 -0700

    Added initial positional tracking to app.
    demo-tag: https://vine.co/v/hJtdvBqEvqd

--- Trackpad/src/main/java/com/phyous/trackpad/TrackpadActivity.java
+++ Trackpad/src/main/java/com/phyous/trackpad/TrackpadActivity.java
@@ -2,16 +2,48 @@ package com.phyous.trackpad;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.view.MotionEventCompat;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class TrackpadActivity extends Activity {
+    private final String DEBUG_TAG = this.getClass().getName();
 
-    @Override
+    private TextView mXpos;
+    private TextView mYpos;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trackpad);
-    }
 
+        mXpos = (TextView) findViewById(R.id.x_pos_initial);
+        mYpos = (TextView) findViewById(R.id.y_pos_initial);
+
+        View myView = findViewById(R.id.trackpad);
+        myView.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = MotionEventCompat.getActionMasked(event);
+
+                switch(action) {
+                    case (MotionEvent.ACTION_MOVE) :
+                        float x = event.getX();
+                        float y = event.getY();
+
+                        String eventStr = String.format("%f:X %f:Y", x, y);
+                        Log.d(DEBUG_TAG, eventStr);
+
+                        mXpos.setText(String.format("%.2f", x));
+                        mYpos.setText(String.format("%.2f", y));
+                        return true;
+                    default :
+                        return true;
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +51,4 @@ public class TrackpadActivity extends Activity {
         getMenuInflater().inflate(R.menu.trackpad, menu);
         return true;
     }
-    
 }
