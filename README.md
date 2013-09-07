d6ca8d568308e469eded16dc1c850619412c87f2
Philip Youssef <pyoussef@twitter.com>
2013-09-07 00:10:44 -0700

    Hooked up App to trackpad-server service running locally.
    demo-tag: https://vine.co/v/hJUhlIJPxFt

---
+++ README.md
@@ -0,0 +1,17 @@
+Trackpad
+====
+
+A trackpad application for controlling your PC mouse from your android device.
+
+Meant to be used in conjunction with [trackpad server](https://github.com/phyous/trackpad-server).
+
+See the evolution here:
+
+Phase 1 - Basic application:
+<iframe class="vine-embed" src="https://vine.co/v/hJtdvBqEvqd/embed/simple" width="480" height="480" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>
+
+Phase 2 - Server setup
+<iframe class="vine-embed" src="https://vine.co/v/hJFUdlYMHBb/embed/simple" width="480" height="480" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>
+
+Phase 3 - Hook server up to application
+<iframe class="vine-embed" src="https://vine.co/v/hJUhlIJPxFt/embed/simple" width="480" height="480" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>
--- Trackpad/src/main/java/com/phyous/trackpad/TrackpadActivity.java
+++ Trackpad/src/main/java/com/phyous/trackpad/TrackpadActivity.java
@@ -1,5 +1,6 @@
 package com.phyous.trackpad;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v4.view.MotionEventCompat;
@@ -9,8 +10,22 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 public class TrackpadActivity extends Activity {
     private final String DEBUG_TAG = this.getClass().getName();
+    private final String TRACKPAD_API_URL = "http://%s:9000/updatePosition?dx=%f&dy=%f";
+    private final String TRACKPAD_API_IP = "192.168.1.2";
+    private final int frameRate = 15;
+    private final float mSensitivity = 70.0f;
+
+    private int currentFrame = 0;
+    private float lastX = 0.0f;
+    private float lastY = 0.0f;
 
     private TextView mXpos;
     private TextView mYpos;
@@ -23,22 +38,33 @@ public class TrackpadActivity extends Activity {
         mYpos = (TextView) findViewById(R.id.y_pos_initial);
 
         View myView = findViewById(R.id.trackpad);
+
         myView.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
+                if (currentFrame % frameRate != 0) {
+                    currentFrame++;
+                    return true;
+                } else {
+                    currentFrame++;
+                }
+
                 int action = MotionEventCompat.getActionMasked(event);
 
-                switch(action) {
-                    case (MotionEvent.ACTION_MOVE) :
+                switch (action) {
+                    case (MotionEvent.ACTION_MOVE):
                         float x = event.getX();
                         float y = event.getY();
-
-                        String eventStr = String.format("%f:X %f:Y", x, y);
-                        Log.d(DEBUG_TAG, eventStr);
-
                         mXpos.setText(String.format("%.2f", x));
                         mYpos.setText(String.format("%.2f", y));
+
+                        float dx = (x - lastX) / mSensitivity;
+                        float dy = (y - lastY) / mSensitivity;
+                        new UpdatePositionTask().execute(String.format(TRACKPAD_API_URL, TRACKPAD_API_IP, dx, dy));
+
+                        lastX = x;
+                        lastY = y;
                         return true;
-                    default :
+                    default:
                         return true;
                 }
             }
@@ -50,5 +76,55 @@ public class TrackpadActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.trackpad, menu);
         return true;
+    }
+
+    /*
+    What's going on here?
+    See: http://developer.android.com/training/basics/network-ops/connecting.html
+   */
+    private class UpdatePositionTask extends AsyncTask<String, Void, String> {
+        public UpdatePositionTask() {
+            super();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                String error = "ERROR";
+                return error;
+            }
+        }
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = convertStreamToString(is);
+            return contentAsString;
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    private static String convertStreamToString(InputStream is) {
+        Scanner s = new Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 }
